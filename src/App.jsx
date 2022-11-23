1613aa3c447d954bf19b989bf0d1830b70fc4ef6
Pierre Liebenberg <pierrelieb@gmail.com>
2022-11-23 19:47:49 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
 import Why from "./components/why/Why";
+import React, { useState, useEffect } from 'react';
+import './app.css'
+import Load from './img/load.svg'
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
+    <div className="container">
+    {loading ? (
+      <div className="loader-container">
+        <div >
+        <img src={Load} alt="Load" className='loader' />
+        </div>
+      </div>
+    ) : (
   <div>
   <Intro/>
   <About/>
   <Why/>
   </div>
+  )}
+  </div>
+
   )
 };
 
